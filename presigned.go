b55c384a56d210483b9e6a961d2c9d9ca30c4887
Michael Caley <kneedeepwater@hotmail.com>
2024-07-22 19:58:46 +0100

    init access s3

--- presigned.go
+++ presigned.go
@@ -1,15 +1,41 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	controllers "presigned.go/controllers/home"
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
+	// initialisers.LoadEnvVars()
 
-	r.GET("/", controllers.Home)
+	// r := gin.Default()
+	// r.LoadHTMLGlob("templates/*")
+	// r.GET("/", controllers.Home)
+	// r.Run(":3000")
 
-	r.Run(":3000")
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+
+	// Get the first page of results for ListObjectsV2 for a bucket
+	output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String("easy-form-static-website"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("first page results:")
+	for _, object := range output.Contents {
+		log.Printf("key=%s size=%d", aws.ToString(object.Key), object.Size)
+	}
 }
