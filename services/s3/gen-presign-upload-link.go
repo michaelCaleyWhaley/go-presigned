8c06206b6337d6423339fd7033ee86a6fbc9afe7
Michael Caley <kneedeepwater@hotmail.com>
2024-07-24 23:13:32 +0100

    generate working presigned url

---
+++ services/s3/gen-presign-upload-link.go
@@ -0,0 +1,30 @@
+package s3Client
+
+import (
+	"context"
+	"log"
+	"time"
+
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+func GeneratePresignedUrl(presignClient *s3.PresignClient) *v4.PresignedHTTPRequest {
+	bucketName := "go-presigned"
+	objectKey := "test_pic.JPG"
+	lifetimeSecs := int64(600)
+
+	request, err := presignClient.PresignPutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	}, func(opts *s3.PresignOptions) {
+		opts.Expires = time.Duration(lifetimeSecs * int64(time.Second))
+	})
+	if err != nil {
+		log.Printf("Couldn't get a presigned request to put %v:%v. Here's why: %v\n",
+			bucketName, objectKey, err)
+	}
+
+	return request
+}
